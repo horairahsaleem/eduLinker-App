ab9263a927beb9432c878cb6fb0c8aa3c288b02c
horairahsaleem <horairahsaleem7864@gmail.com>
2025-09-05 10:35:41 +0500

    removing extra reducers

--- src/redux/Reducers/userReducer.js
+++ src/redux/Reducers/userReducer.js
@@ -22,55 +22,5 @@ export const userReducer = createReducer(
       state.error = action.payload;
     },
 
-    registerRequest: state => {
-      state.loading = true;
-    },
-    registerSuccess: (state, action) => {
-      state.loading = false;
-      state.isAuthenticated = true;
-      state.user = action.payload.user;
-      state.message = action.payload.message;
-    },
-    registerFail: (state, action) => {
-      state.loading = false;
-      state.isAuthenticated = false;
-      state.error = action.payload;
-    },
-
-    logoutRequest: state => {
-      state.loading = true;
-    },
-    logoutSuccess: (state, action) => {
-      state.loading = false;
-      state.isAuthenticated = false;
-      state.user = null;
-      state.message = action.payload;
-    },
-    logoutFail: (state, action) => {
-      state.loading = false;
-      state.isAuthenticated = true;
-      state.error = action.payload;
-    },
-
-    loadUserRequest: state => {
-      state.loading = true;
-    },
-    loadUserSuccess: (state, action) => {
-      state.loading = false;
-      state.isAuthenticated = true;
-      state.user = action.payload;
-    },
-    loadUserFail: (state, action) => {
-      state.loading = false;
-      state.isAuthenticated = false;
-      state.error = action.payload;
-    },
-
-    clearError: state => {
-      state.error = null;
-    },
-    clearMessage: state => {
-      state.message = null;
-    },
   }
 );
