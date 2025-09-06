9a964bbe7a76f4f8431c4f3b5548a85ab26844ad
horairahsaleem <horairahsaleem7864@gmail.com>
2025-09-06 15:19:31 +0500

    wrapping Store & updating the reducer to newer version

--- src/index.js
+++ src/index.js
@@ -3,17 +3,19 @@ import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
 import { ChakraProvider, theme } from '@chakra-ui/react';
-
+import store from './redux/store.js'
+import { Provider } from "react-redux";
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
 root.render(
   <StrictMode>
+    <Provider store={store}>
       <ChakraProvider theme={theme}>
         <ColorModeScript />
         <App />
       </ChakraProvider>
-    
+    </Provider>
   </StrictMode>
 );
 
--- src/redux/Reducers/userReducer.js
+++ src/redux/Reducers/userReducer.js
@@ -1,26 +1,27 @@
 import { createReducer } from '@reduxjs/toolkit';
 
-export const userReducer = createReducer(
-  { loading: false,
+const initialState = {
+  loading: false,
   isAuthenticated: false,
   user: null,
   message: null,
-  error: null,},
-  {
-    loginRequest: state => {
+  error: null,
+};
+
+export const userReducer = createReducer(initialState, (builder) => {
+  builder
+    .addCase('loginRequest', (state) => {
       state.loading = true;
-    },
-    loginSuccess: (state, action) => {
+    })
+    .addCase('loginSuccess', (state, action) => {
       state.loading = false;
       state.isAuthenticated = true;
       state.user = action.payload.user;
       state.message = action.payload.message;
-    },
-    loginFail: (state, action) => {
+    })
+    .addCase('loginFail', (state, action) => {
       state.loading = false;
       state.isAuthenticated = false;
       state.error = action.payload;
-    },
-
-  }
-);
+    });
+});
