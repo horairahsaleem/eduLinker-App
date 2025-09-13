20ebee704a5814412511377d18ff7815dd2d7837
horairahsaleem <horairahsaleem7864@gmail.com>
2025-09-13 15:24:12 +0500

    Forget password page compltely working

--- src/components/Auth/ForgetPassword.jsx
+++ src/components/Auth/ForgetPassword.jsx
@@ -1,11 +1,35 @@
 import { Heading, VStack,Input,Button,Container } from '@chakra-ui/react'
-import React, { useState } from 'react'
+import React, { useState , useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { forgetPassword } from '../../redux/Actions/profileActions.js';
+import toast from 'react-hot-toast';
+
 
 const ForgetPassword = () => {
-    const {email,setEmail} =useState()
+    const [email,setEmail] = useState('')
+    
+  const { loading, message, error } = useSelector(state => state.profile);
+
+  const dispatch = useDispatch();
+  const submitHandler = e => {
+    e.preventDefault();
+    dispatch(forgetPassword(email));
+  };
+
+  useEffect(() => {
+    if (error) {
+      toast.error(error);
+      dispatch({ type: 'clearError' });
+    }
+    if (message) {
+      toast.success(message);
+      dispatch({ type: 'clearMessage' });
+    }
+  }, [dispatch, error, message]);
+
   return (
     <Container py={'16'} h={'90vh'}>
-        <form>
+        <form onSubmit={submitHandler}>
             <Heading children='Forget Password' my={'16'} textTransform={'uppercase'} textAlign={['center','left']} />
             <VStack>
             <Input required value={email} onChange={e => setEmail(e.target.value)}
