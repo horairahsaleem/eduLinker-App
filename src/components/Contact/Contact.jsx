2b7aab155d1807390a7a9a600aea22349c224540
horairahsaleem <horairahsaleem7864@gmail.com>
2025-09-17 07:07:35 +0500

    contact us page integrated

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,16 +1,43 @@
 import { Box, Button, Container, FormLabel, Heading, Input, Textarea, VStack } from '@chakra-ui/react'
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux';
+import { contactUs } from '../../redux/Actions/otherActions';
+import toast from 'react-hot-toast';
+import { useEffect } from 'react';
 
 const Contact = () => {
     const [name,setName]=useState()
     const [email,setEmail]=useState()
     const [message,setMessage]=useState()
+          const dispatch = useDispatch();
+      const {
+        loading,
+        error,
+        message: stateMessage,
+      } = useSelector(state => state.other);
+    
+      const submitHandler = e => {
+        e.preventDefault();
+        dispatch(contactUs(name, email, message));
+      };
+    
+      useEffect(() => {
+        if (error) {
+          toast.error(error);
+          dispatch({ type: 'clearError' });
+        }
+    
+        if (stateMessage) {
+          toast.success(stateMessage);
+          dispatch({ type: 'clearMessage' });
+        }
+      }, [dispatch, error, stateMessage]);
   return (
     <Container h='92vh'>
         <VStack h={'full'} justifyContent={'center'} spacing={'10'}>
             <Heading children='CONTACT US'/>
-            <form style={{width:'100%'}}>
+            <form onSubmit={submitHandler} style={{width:'100%'}}>
             <Box my={'4'}>
 
 <FormLabel htmlFor='name' children='Name'/>
@@ -40,7 +67,7 @@ focusBorderColor='yellow.500'
                 </Box>
                
                 
-                <Button type='submit' my={'4'} colorScheme='yellow'>
+                <Button  isLoading={loading} type='submit' my={'4'} colorScheme='yellow'>
                     Send Mail
                 </Button>
                 <Box my={'4'}>
