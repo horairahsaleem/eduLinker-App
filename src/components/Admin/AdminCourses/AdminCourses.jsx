f3b9d6d0b27908e8d41ab1687f7fb023e88daf7c
horairahsaleem <horairahsaleem7864@gmail.com>
2025-09-20 11:00:34 +0500

    Admin page all functions created methods dispated

--- src/components/Admin/AdminCourses/AdminCourses.jsx
+++ src/components/Admin/AdminCourses/AdminCourses.jsx
@@ -4,35 +4,70 @@ import cursor from '../../../assets/images/cursor.png'
 import Sidebar from '../Sidebar'
 import { RiDeleteBin7Fill } from 'react-icons/ri'
 import CourseModal from './CourseModal'
+import { useEffect ,useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  getAllCourses,
+  getCourseLectures,
+} from '../../../redux/Actions/courseActions.js';
+import {
+  addLecture,
+  deleteCourse,
+  deleteLecture,
+} from '../../../redux/Actions/adminActions.js';
+import toast from 'react-hot-toast';
 
 const AdminCourses = () => {
-  const course = [{
-     _id:"yy66iotyrew2211dc",
-      title:"React Course               ",
-      category:"Web Development",
-      createdBy:"Horairah Saleem",
-      views:123,
-      numOfVedios:12,
-      poster:{
-       url:'https://cdn.pixabay.com/photo/2016/10/26/19/00/domain-names-1772242_1280.jpg'
-      }
-    }
-  ]
+   const { courses, lectures } = useSelector(state => state.course);
+
+  const { loading, error, message } = useSelector(state => state.admin);
+
+  const dispatch = useDispatch();
+
   const {isOpen,onClose,onOpen} = useDisclosure()
-  const courseDetailsHandler=(userId)=>{
-onOpen()
+const [courseId, setCourseId] = useState('');
+  const [courseTitle, setCourseTitle] = useState('');
+
+   const courseDetailsHandler = (courseId, title) => {
+    dispatch(getCourseLectures(courseId));
+    onOpen();
+    setCourseId(courseId);
+    setCourseTitle(title);
+  };
+ const deleteButtonHandler = courseId => {
+    console.log(courseId);
+    dispatch(deleteCourse(courseId));
+  };
+
+  const deleteLectureButtonHandler = async (courseId, lectureId) => {
+    await dispatch(deleteLecture(courseId, lectureId));
+    dispatch(getCourseLectures(courseId)); 
+  };
+  const addLectureHandler= async (e,courseId,title,description,video)=>{
+e.preventDefault();
+    const myForm = new FormData();
+
+    myForm.append('title', title); 
+    myForm.append('description', description);
+    myForm.append('file', video);
+
+    await dispatch(addLecture(courseId, myForm));
+    dispatch(getCourseLectures(courseId));    
   }
-  const deleteButtonHandler=(userId)=>{
-    console.log(userId)
-  }
-  const deleteLectureButtonHandler=(courseId,lectureId)=>{
-    console.log(courseId)
-    console.log(lectureId)
-  }
-  const addLectureHandler=(e,courseId,title,description,video)=>{
-    e.prevent.default()
-    
-  }
+  
+  useEffect(() => {
+    if (error) {
+      toast.error(error);
+      dispatch({ type: 'clearError' });
+    }
+
+    if (message) {
+      toast.success(message);
+      dispatch({ type: 'clearMessage' });
+    }
+
+    dispatch(getAllCourses());
+  }, [dispatch, error, message, onClose]);
 
   return (
     <Grid
@@ -44,7 +79,7 @@ onOpen()
     }}
     >
         <Box p={['0','8']} overflow={'auto'}>
-        <Heading textTransform={'uppercase'} textAlign={['center','left']} my={'16'} children='All  Users'/>
+        <Heading textTransform={'uppercase'} textAlign={['center','left']} my={'16'} children='All  Courses'/>
         <TableContainer w={['100vw','full']}>
           <Table variant={'simple'} size={'lg'}>
             <TableCaption>All available Courses in the database</TableCaption>
@@ -61,7 +96,7 @@ onOpen()
   </Tr>
 </Thead>
 <Tbody>
-  {course.map(item =>(
+  {courses.map(item =>(
     <Row courseDetailsHandler={courseDetailsHandler} deleteButtonHandler={deleteButtonHandler}  item={item} key={item._id} />
   ))}
 
@@ -83,7 +118,7 @@ id={'sdsdsadsad'}
   )
 }
 
- function Row({item,courseDetailsHandler,deleteButtonHandler}){
+ function Row({item,courseDetailsHandler,deleteButtonHandler,loading}){
 
  return( 
   <Tr>
@@ -97,7 +132,7 @@ id={'sdsdsadsad'}
 
 <Td isNumeric>
 <HStack>
-<Button variant={'outline'} color={'purple.500'} onClick={() => courseDetailsHandler (item._id)}>
+<Button variant={'outline'} color={'purple.500'} onClick={() => courseDetailsHandler (item._id , item.title)}>
   View Lectures
 
 </Button>
